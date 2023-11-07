c4f8c0921bc67225706bb1575423f6027f353f80
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2023-11-07 18:00:35 +0100

    Es crea l'arxiu tipus_variables.php corresponent al Nivell-1 Exercici-1 amb cuatre variables amb tipus de dades diferents

---
+++ Nivell-1/Exercici-1/tipus_variables.php
@@ -0,0 +1,11 @@
+<?php
+
+$enter = 7;
+$doble = 4.35;
+$cadena = "Desde PHP";
+$booleano = true;
+
+echo $enter . "<br />";
+echo $doble . "<br />";
+echo $cadena . "<br />";
+echo $booleano . "<br />";
