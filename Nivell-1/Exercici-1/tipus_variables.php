020ffcd0add4bcd8390c34706813128bc3c6868b
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2023-11-07 18:12:53 +0100

    Dintre l'arxiu tipus_variables.php es defineix una constant NOMSENCER y es mostra per pantalla en format títol com un encabezat de primer nivell

--- Nivell-1/Exercici-1/tipus_variables.php
+++ Nivell-1/Exercici-1/tipus_variables.php
@@ -9,3 +9,18 @@ echo $enter . "<br />";
 echo $doble . "<br />";
 echo $cadena . "<br />";
 echo $booleano . "<br />";
+
+define ( "NOMSENCER" , "Jesús Martín Blanco");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo NOMSENCER ?></h1>
+</body>
+</html>
