8d7d807db91926ff4662e45d83141bac938a7609
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2023-11-07 18:34:59 +0100

    Es crea l'arxiu salutacio.php corresponent a l'exercici 2 del NIvell-1

---
+++ Nivell-1/Exercici-2/salutacio.php
@@ -0,0 +1,15 @@
+<?php
+
+$salutacio = "Hello, World!";
+
+echo $salutacio . "<br />";
+
+echo strtoupper($salutacio) . "<br />";
+
+echo "Hello, World! conté " . strlen($salutacio) . " caracters.<br />";
+
+echo strrev($salutacio) . "<br />";
+
+$str = "Aquest és el curs de PHP";
+
+echo $salutacio . " " . $str . "<br />";
